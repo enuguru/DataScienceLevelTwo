4b3835ee4ef07508c3482264137467f3b3509ee1
karthik <karthikkannan@gmail.com>
2021-03-12 08:15:45 +0530

    new case studies

--- timeseries_seethis/timeseries_components/seasonal_decompose.py
+++ timeseries_seethis/timeseries_components/seasonal_decompose.py
@@ -3,7 +3,7 @@ from pandas import read_csv
 from matplotlib import pyplot
 import statsmodels as sm
 from statsmodels.tsa.seasonal import seasonal_decompose
-series = read_csv('AirPassengers.csv', header=0, index_col=0)
+series = read_csv('../Data/AirPassengers.csv', header=0, index_col=0)
 result = seasonal_decompose(np.asarray(series), freq=3)
 #result = seasonal_decompose(series, model='multiplicative')
 result.plot()
